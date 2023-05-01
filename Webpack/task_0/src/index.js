734c68a108a6183ae9311e3fe83f5d06d1f3adc3
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-01 16:22:24 -0500

    started

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+const body = $('body');
+
+body.append("Holberton Dashboard");
+body.append("Dashboard data for the students");
+body.append("Copyright - Holberton School");
