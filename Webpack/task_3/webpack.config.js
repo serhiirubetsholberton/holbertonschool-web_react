47419b2533ab452419afaedd309d17539556c40d
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-02 15:58:59 -0500

    fixed settings

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,52 +1,54 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'js/dashboard_main.js'),
+  devtool: 'inline-source-map',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
   output: {
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
   },
   optimization: {
     splitChunks: {
       chunks: 'all',
     },
   },
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
   performance: {
+    hints: false,
     maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
   },
   devServer: {
     contentBase: path.join(__dirname, './public'),
     compress: true,
     port: 8564,
   },
-  plugins: [
-    new HTMLWebpackPlugin({
-      filename: 'public/index.html'
-    }),
-    new CleanWebpackPlugin()
-  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(jpg|gif|png|jpeg|svg)$/i,
+        test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          "file-loader",
+          'file-loader',
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
-              bypassOnDebug: true,
-              disable: true,
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
           },
         ],
-      }
-    ]
-  }
-}
+      },
+    ],
+  },
+};
