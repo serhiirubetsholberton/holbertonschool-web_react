7b03245292bc2e7325c820bbd5d460d702f6e0a4
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-05-02 15:23:50 -0500

    added css

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from "jquery";
+import _ from "lodash";
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let clicksCount = 0;
+
+function updateCounter(e) {
+  $('#count').html(`${++clicksCount} clicks on the button`);
+}
+
+const debouncedClicker = _.debounce(updateCounter, 500);
+$('button').on('click', debouncedClicker);
+console.log($('#count'))
